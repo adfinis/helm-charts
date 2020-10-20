f66db094dad9bcceec443b2e2116a8ade149167b
Lucas Bickel <lucas.bickel@adfinis.com>
2020-10-20 16:11:30 +0200

    chore(ci): replace deprecated add-path command (#89)

--- hack/ci/github/setup/pre-commit.sh
+++ hack/ci/github/setup/pre-commit.sh
@@ -10,7 +10,7 @@ mkdir -p $ADFINIS_CHARTS_TMP_BIN
 # Install deps
 source hack/sh/deps/helm-docs.sh
 source hack/sh/deps/gomplate.sh
-echo "::add-path::$ADFINIS_CHARTS_TMP_BIN"
+echo $ADFINIS_CHARTS_TMP_BIN >> $GITHUB_PATH
 
 # Set PY
-echo "::set-env name=PY::$(python -c 'import hashlib, sys;print(hashlib.sha256(sys.version.encode()+sys.executable.encode()).hexdigest())')"
+echo "PY=$(python -c 'import hashlib, sys;print(hashlib.sha256(sys.version.encode()+sys.executable.encode()).hexdigest())')" >> $GITHUB_ENV
