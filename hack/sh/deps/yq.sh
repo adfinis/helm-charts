b816ec18a39ad3a0084b7b32b71e6b8465ac8538
Toni Tauro <toni.tauro@adfinis.com>
2021-03-19 15:36:13 +0100

    feat(release.yml): add repos from `hack/chart-testing/ct.yaml`
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

---
+++ hack/sh/deps/yq.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -e
+
+source hack/sh/_functions.sh
+
+readonly yq_target=$ADFINIS_CHARTS_TMP_BIN
+readonly yq_version=$ADFINIS_CHARTS_YQ_VERSION
+readonly yq_bin=$yq_target/yq
+
+readonly yq_url="https://github.com/mikefarah/yq/releases/download/v${yq_version}/yq_linux_amd64"
+
+set +e
+
+if [[ ! -x `which yq` ]]; then
+   curl -L -o $yq_bin $yq_url
+   chmod +x $yq_bin
+   [[ ! -x `which yq` ]] && err "Could not install yq binary."
+fi
