8f69ffae53a0d5cc8bbe735f5acf1ac8821aca17
Toni Tauro <toni.tauro@adfinis.com>
2021-03-22 09:23:25 +0100

    fix(yq.sh): check for binary (#203)
    Signed-off-by: Toni Tauro <toni.tauro@adfinis.com>

--- hack/sh/deps/yq.sh
+++ hack/sh/deps/yq.sh
@@ -8,12 +8,12 @@ readonly yq_target=$ADFINIS_CHARTS_TMP_BIN
 readonly yq_version=$ADFINIS_CHARTS_YQ_VERSION
 readonly yq_bin=$yq_target/yq
 
-readonly yq_url="https://github.com/mikefarah/yq/releases/download/v${yq_version}/yq_linux_amd64"
+readonly yq_url="https://github.com/mikefarah/yq/releases/download/v$yq_version/yq_linux_amd64"
 
 set +e
 
 if [[ ! -x `which yq` ]]; then
    curl -L -o $yq_bin $yq_url
    chmod +x $yq_bin
-   [[ ! -x `which yq` ]] && err "Could not install yq binary."
+   [[ ! -x $yq_bin ]] && err "Could not install yq binary."
 fi
