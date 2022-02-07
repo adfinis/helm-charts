6cd55e04a69362797470c402c15baf75b3c5dad2
Toni Tauro <toni.tauro@adfinis.com>
2022-02-07 14:18:09 +0100

    Update hack/pluto.sh
    Co-authored-by: Lucas Bickel <116588+hairmare@users.noreply.github.com>

--- hack/pluto.sh
+++ hack/pluto.sh
@@ -5,6 +5,11 @@
 set -e
 
 source hack/sh/rc.sh
+source hack/sh/_functions.sh
 source hack/sh/deps/pluto.sh
 
-helm template $1 | pluto detect -
+chart_dir=$1
+
+[[ -d $chart_dir ]] || err "No chart dir given. Usage: hack/pluto.sh chart/name"
+
+helm template $chart_dir | pluto detect -
