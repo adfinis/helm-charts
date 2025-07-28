a7e069ffac50c359d07907f36a30e3c4578b3f7b
Felix Niederer <felix.niederer@adfinis.com>
2025-07-28 15:45:36 +0200

    chore: updating argocd, running pre commits

--- open-issues.sh
+++ open-issues.sh
@@ -6,13 +6,13 @@ ASSIGNEE="Xelef2000"
 
 ls charts/*apps/values.yaml -1 | grep -v "sentry-apps" | while read valueFile; do
   echo "Processing file: $valueFile"
-  
+
   egrep -o "^[a-zA-Z]+:$" "$valueFile" | while read -r app; do
     name=$(echo "$app" | tr -d ":")
     repoURL=$(yq e ".${name}.repoURL" "$valueFile")
     targetRevision=$(yq e ".${name}.targetRevision" "$valueFile")
     chart=$(yq e ".${name}.chart" "$valueFile")
-    
+
     curVers=$(curl --silent -qk "${repoURL}/index.yaml" | yq e ".entries.${chart}[0].version" -)
 
     echo "----------------------------------------"
@@ -20,21 +20,21 @@ ls charts/*apps/values.yaml -1 | grep -v "sentry-apps" | while read valueFile; d
     echo "  -> Your Version (targetRevision): ${targetRevision}"
     echo "  -> Latest Version Available:      ${curVers}"
     echo "----------------------------------------"
-    
+
     if [[ "$curVers" != "$targetRevision" && "$curVers" != "null" ]]; then
-      
+
       read -r -d '' body <<EOF
 New version available for **${name}**!<br/><br/>Current \`targetRevision\` set: \`${targetRevision}\`<br/>New version available: \`${curVers}\`<br/><br/>Please consider creating a PR to update the \`targetRevision\` in this file: [${valueFile}](https://github.com/${REPO}/tree/main/${valueFile})<br/><br/>Thanks.
 EOF
-      
+
       echo "✅ Found new version for ${name}! Creating and assigning issue..."
-      
+
       json_payload=$(jq -n \
         --arg title "New Version Available: ${name} ${curVers}" \
         --arg body "$body" \
         --arg assignee "$ASSIGNEE" \
         '{title: $title, body: $body, assignees: [$assignee]}')
-        
+
       curl --silent -X "POST" "https://api.github.com/repos/${REPO}/issues" \
         -H "Accept: application/vnd.github.v3+json" \
         -H "Authorization: token ${GITHUB_PAT}" \
